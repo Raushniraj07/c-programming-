6c825a7f999b9fa47e6e2650c293a7c9787e3403
Raushni Raj <112066251+Raushniraj07@users.noreply.github.com>
2022-09-27 10:28:00 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i, j,a;
+    printf("enter the number of lines= ");
+    scanf("%d",&a);
+    for(i=a;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("0.%d",i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
